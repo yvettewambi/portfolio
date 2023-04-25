abc4fc84371ba0bf59d3bb6de191c374c7665536
Yvette Wambi Nerima <yvettebelle2710@gmail.com>
2023-04-25 18:44:40 +0300

    Initial files

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const NavBar = () => {
+  const [onLink, setLink] = useState("home");
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Skills</Nav.Link>
+            <Nav.Link href="#link">Project</Nav.Link>
+          </Nav>
+          <span className="nav-text">
+            <div className="icon">
+              <a href="#">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="" />
+              </a>
+            </div>
+            <button className="vvd">
+              <span>Let’s Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
